a217054352a077a8169f9818a9bdda72ed89677c
Hector Vadillo <vadillo.hector@gmail.com>
2016-03-11 18:52:19 +0100

    Test-ak eratuta, baina ez inplementatua.

---
+++ test/org/si/dragamina/GelaMinaTest.java
@@ -0,0 +1,34 @@
+package org.si.dragamina;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GelaMinaTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGelaIreki() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEguneratu() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGelaMina() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ test/org/si/dragamina/GelaZenbakiaTest.java
@@ -0,0 +1,34 @@
+package org.si.dragamina;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GelaZenbakiaTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGelaIreki() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEguneratu() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGelaZenbakia() {
+		fail("Not yet implemented");
+	}
+
+}
