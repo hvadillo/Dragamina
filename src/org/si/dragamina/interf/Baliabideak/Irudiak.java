06836808d6525894f1690fe97f566d2fe847438a
AimUp <ugarteaimar@gmail.com>
2016-04-17 00:30:10 +0200

    Jar fitxategia sortzean irudiekin zegoen arazoa konpondu.

--- src/org/si/dragamina/interf/Baliabideak/Irudiak.java
+++ src/org/si/dragamina/interf/Baliabideak/Irudiak.java
@@ -22,48 +22,48 @@ public class Irudiak {
 	
 	private static void zenbakiakKargatu(){
 		zenbakiak[0] = null;
-		zenbakiak[1] = new ImageIcon("Irudiak/1.png");
-		zenbakiak[2] = new ImageIcon("Irudiak/2.png");
-		zenbakiak[3] = new ImageIcon("Irudiak/3.png");
-		zenbakiak[4] = new ImageIcon("Irudiak/4.png");
-		zenbakiak[5] = new ImageIcon("Irudiak/5.png");
-		zenbakiak[6] = new ImageIcon("Irudiak/6.png");
-		zenbakiak[7] = new ImageIcon("Irudiak/7.png");
-		zenbakiak[8] = new ImageIcon("Irudiak/8.png");
+		zenbakiak[1] = new ImageIcon(Irudiak.class.getResource("1.png"));
+		zenbakiak[2] = new ImageIcon(Irudiak.class.getResource("2.png"));
+		zenbakiak[3] = new ImageIcon(Irudiak.class.getResource("3.png"));
+		zenbakiak[4] = new ImageIcon(Irudiak.class.getResource("4.png"));
+		zenbakiak[5] = new ImageIcon(Irudiak.class.getResource("5.png"));
+		zenbakiak[6] = new ImageIcon(Irudiak.class.getResource("6.png"));
+		zenbakiak[7] = new ImageIcon(Irudiak.class.getResource("7.png"));
+		zenbakiak[8] = new ImageIcon(Irudiak.class.getResource("8.png"));
 	}
 	
 	private static void smileyKargatu(){
-		smiley[0] = new ImageIcon("Irudiak/smile.png");
-		smiley[1] = new ImageIcon("Irudiak/win.png");
-		smiley[2] = new ImageIcon("Irudiak/loose.png");
-		smiley[3] = new ImageIcon("Irudiak/click.png");
-		smiley[4] = new ImageIcon("Irudiak/cry.png");
+		smiley[0] = new ImageIcon(Irudiak.class.getResource("smile.png"));
+		smiley[1] = new ImageIcon(Irudiak.class.getResource("win.png"));
+		smiley[2] = new ImageIcon(Irudiak.class.getResource("loose.png"));
+		smiley[3] = new ImageIcon(Irudiak.class.getResource("click.png"));
+		smiley[4] = new ImageIcon(Irudiak.class.getResource("cry.png"));
 	}
 	
 	private static void blokeakKargatu(){
-		bloke[0] = new ImageIcon("Irudiak/close.png");
-		bloke[1] = new ImageIcon("Irudiak/open.png");
-		bloke[2] = new ImageIcon("Irudiak/mine.png");
-		bloke[3] = new ImageIcon("Irudiak/flag.png");
-		bloke[4] = new ImageIcon("Irudiak/think.png");
-		bloke[5] = new ImageIcon("Irudiak/noflag.png");
+		bloke[0] = new ImageIcon(Irudiak.class.getResource("close.png"));
+		bloke[1] = new ImageIcon(Irudiak.class.getResource("open.png"));
+		bloke[2] = new ImageIcon(Irudiak.class.getResource("mine.png"));
+		bloke[3] = new ImageIcon(Irudiak.class.getResource("flag.png"));
+		bloke[4] = new ImageIcon(Irudiak.class.getResource("think.png"));
+		bloke[5] = new ImageIcon(Irudiak.class.getResource("noflag.png"));
 	}
 	
 	private static void kontadoreaKargatu(){
-		kontadore[0] = new ImageIcon("Irudiak/display0.png");
-		kontadore[1] = new ImageIcon("Irudiak/display1.png");
-		kontadore[2] = new ImageIcon("Irudiak/display2.png");
-		kontadore[3] = new ImageIcon("Irudiak/display3.png");
-		kontadore[4] = new ImageIcon("Irudiak/display4.png");
-		kontadore[5] = new ImageIcon("Irudiak/display5.png");
-		kontadore[6] = new ImageIcon("Irudiak/display6.png");
-		kontadore[7] = new ImageIcon("Irudiak/display7.png");
-		kontadore[8] = new ImageIcon("Irudiak/display8.png");
-		kontadore[9] = new ImageIcon("Irudiak/display9.png");
+		kontadore[0] = new ImageIcon(Irudiak.class.getResource("display0.png"));
+		kontadore[1] = new ImageIcon(Irudiak.class.getResource("display1.png"));
+		kontadore[2] = new ImageIcon(Irudiak.class.getResource("display2.png"));
+		kontadore[3] = new ImageIcon(Irudiak.class.getResource("display3.png"));
+		kontadore[4] = new ImageIcon(Irudiak.class.getResource("display4.png"));
+		kontadore[5] = new ImageIcon(Irudiak.class.getResource("display5.png"));
+		kontadore[6] = new ImageIcon(Irudiak.class.getResource("display6.png"));
+		kontadore[7] = new ImageIcon(Irudiak.class.getResource("display7.png"));
+		kontadore[8] = new ImageIcon(Irudiak.class.getResource("display8.png"));
+		kontadore[9] = new ImageIcon(Irudiak.class.getResource("display9.png"));
 	}
 	
 	private static void ikonoaKargatu(){
-		ImageIcon image = new ImageIcon("Irudiak/icon.png");  	
+		ImageIcon image = new ImageIcon(Irudiak.class.getResource("icon.png"));	
 		ikonoa = image.getImage(); 
 	}
 }
--- src/org/si/dragamina/logic/Main.java
+++ src/org/si/dragamina/logic/Main.java
@@ -2,13 +2,11 @@ package org.si.dragamina.logic;
 
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
-
 import org.si.dragamina.interf.Leihoa;
 
 public abstract class Main {
 	
 	public static void main(String[] args) {
-		
 		try {
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		} catch (InstantiationException e) {
